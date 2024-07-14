e8aff97e6ad90e50e9cc67c40f0ab1586565fd4b
cristian-air-m2 <cristian_cortes_ortiz@hotmail.com>
2024-07-14 04:13:34 -0500

    student router updated with the api key middleware

--- src/routes/studentsRouter.js
+++ src/routes/studentsRouter.js
@@ -3,6 +3,8 @@ import { Router } from "express";
 // Import the middleware to verify tokens from the authentication app
 import { authAppVerifyToken } from
 '../middlewares/tokenHandlers/authAppTokenHandler.js';
+// Import the middleware to verify the API key from the client app
+import { checkApiKey } from '../middlewares/apiAuthHandler.js';
 // Import the controller function to authenticate students
 import { authenticateStudent } from '../controllers/students/authenticate.js';
 
@@ -11,8 +13,11 @@ const router = Router();
 
 // Define a POST route for student authentication
 router.post(
-  // Route path with a parameter for student fingerprint id as a token (jwt)
-  '/auth/:jwt',
+  // Route path to authenticate a student fingerprint id
+  '/auth',
+  // Middleware to verify the API key sended by the client before
+  // proceeding to the controller
+  checkApiKey,
   // Middleware to verify the token before proceeding to the controller
   authAppVerifyToken,
    // Controller function to authenticate the student
