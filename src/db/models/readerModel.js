895b9bd2bfa9129d6587dc1ef53bbf44e3bb320a
cristian-air-m2 <cristian_cortes_ortiz@hotmail.com>
2024-08-15 21:30:28 -0500

    reader model created

---
+++ src/db/models/readerModel.js
@@ -0,0 +1,55 @@
+// Import the sequelize instance from the database connection library
+import { sequelize } from '../../libraries/DBConnection.js';
+// Import DataTypes from sequelize to define column types
+import { DataTypes } from 'sequelize';
+
+// Define the name of the students table
+export const READER_TABLE = 'readers';
+
+// Define the Reader model
+export const Reader = sequelize.define(READER_TABLE, {
+
+  // Define the 'id' column as an integer
+  id: {
+    // Integer type with a length of 10
+    type: DataTypes.INTEGER(10),
+    // This field cannot be null
+    allowNull: false,
+    // This field is the primary key
+    primaryKey: true,
+    // This field must be unique across all records
+    unique: true
+  },
+
+  // Define the 'username' column
+  username: {
+    // Text type with a maximum length of 10 characters
+    type: DataTypes.TEXT(10),
+    // This field cannot be null
+    allowNull: false,
+    // This field does not need to be unique
+    unique: false,
+  },
+
+  // Define the 'password' column
+  password: {
+    // Text type with a maximum length of 30 characters
+    type: DataTypes.STRING(30),
+    // This field cannot be null
+    allowNull: false,
+    // This field does not need to be unique
+    unique: false,
+  },
+
+},
+{
+  // Additional model options
+  // Pass the sequelize instance
+  sequelize,
+  // Specify the table name
+  tableName: READER_TABLE,
+  // Specify the model name
+  modelName: 'Reader',
+  // Enable automatic timestamp fields (createdAt, updatedAt)
+  timestamps: true
+});
