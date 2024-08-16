0f20e12d95bec36231bccd4b53325130107991c9
cristian-air-m2 <cristian_cortes_ortiz@hotmail.com>
2024-08-15 20:41:46 -0500

    reader user RegEx created

---
+++ src/utils/RegEx/readerRegEx.js
@@ -0,0 +1,15 @@
+// The pattern ensures the username contains only letters
+// (both uppercase and lowercase)
+// and is between 3 to 10 characters long
+export const readerUsername = /^[a-zA-Z]{3,10}$/;
+
+// Regex pattern to validate the user password
+// The pattern ensures the password contains
+// letters (both uppercase and lowercase), numbers, and characters
+// and is between 5 to 30 characters long
+export const studentSecondName = /^(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]{5,30}$/;
+
+// Regex pattern to validate the user id
+// The pattern ensures the unique identifier contains only umbers
+// and is between 1 to 10 digits long
+export const readerId = /^\d{1,10}$/
